e65d0b16a7c8a224ea854f401c1857f808e5a13b
NaveenAN1204 <159796265+NaveenAN1204@users.noreply.github.com>
2025-01-19 21:06:35 +0530

    Add files via upload

---
+++ @i@p6/_primary.vhd
@@ -0,0 +1,10 @@
+library verilog;
+use verilog.vl_types.all;
+entity IP6 is
+    port(
+        clk             : in     vl_logic;
+        rst             : in     vl_logic;
+        \IN\            : in     vl_logic_vector(19 downto 0);
+        \out\           : out    vl_logic_vector(19 downto 0)
+    );
+end IP6;
